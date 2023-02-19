a0c3924c960634f98c59702123555b2d896e2357
dana971 <nac.dara@gmail.com>
2023-02-19 14:31:28 +0100

    MAJ display house fonctionnel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import FicheLogement from "./pages/Fiche-Logement";
 import About from "./pages/A-Propos";
 import Error404 from "./pages/404";
+import Banner from "./components/Banner";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
 import "../src/utils/index.scss";
 import "./assets/fonts/Montserrat-Regular.ttf";
-import Banner from "./components/Banner";
-
 
 
 
