5753fd7b1b4afd7acaf590a867417be95e08810f
dana971 <nac.dara@gmail.com>
2023-02-19 15:37:11 +0100

    Pixel Perfect page acceuil

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,8 +1,13 @@
+import "../../assets/images/404.svg"
+import {NavLink} from "react-router-dom";
+
 function Error404 (){
 
     return(
         <div>
-            <h1> Test Error</h1>
+            <img src="./../assets/images/404.svg" alt="Message d'erreur 404"/>
+            <span>Oups ! La page que vous demandez n'existe pas !</span>
+            <div>Retourner sur la page d'accueil</div>
         </div>
     )
 }
