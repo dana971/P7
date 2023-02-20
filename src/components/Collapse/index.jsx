86c3687e7f167797a757bd6dd62817ae2fc301fe
dana971 <nac.dara@gmail.com>
2023-02-20 13:17:13 +0100

    Page About- Collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,6 +1,10 @@
 import "./collapse.scss"
 import {useState} from "react";
-function Collapse(){
+import arrow from "../../assets/images/arrow.svg"
+
+
+
+function Collapse({fiabilite}){
 
     const [open, setOpen ] = useState(false);
     const handleOpen = () => {
@@ -10,33 +14,16 @@ function Collapse(){
     return(
 
     <div className="dropdown-body">
-        <button className="dropdown-long" onClick={handleOpen}>Title</button>
+        <div className="dropdown-head" onClick={handleOpen}>
+            <button className="dropdown-btn" >Title</button>
+            <img src={arrow}/>
+        </div>
         {
         open ?
-            <div className="is-open">Les annonces postées sur Kasa garantissent une fiabilité totale.
-                Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</div>
+            <div className="is-open">{fiabilite}</div>
         :
             <div className="is-close"></div> }
     </div>
-
-        /*
-        <select className="dropdown-long">
-            <option>La bienveillance fait partie des valeurs fondatrices de Kasa.
-                Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</option>
-        </select>
-
-        <select className="dropdown-long">
-            <span>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.
-                N'hésitez pas à nous contacter si vous avez la moindre question.</span>
-        </select>
-
-        <select className="dropdown-long">
-            <span>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
-                chaque logement correspond aux critères de sécurité établis par nos services.
-                En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés.
-                Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</span>
-        </select> */
-
     )
 }
 export default Collapse
